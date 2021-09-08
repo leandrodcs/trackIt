619e5d0c0aa5fccfe124a8326329fe40fe54f670
leandrodcs <eng.leandrodcs@gmail.com>
2021-09-08 15:23:48 -0300

    Register page layout added

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import '../reset.css';
+import Cadastro from './Cadastro';
 import Login from './Login';
 
 export default function App() {
@@ -7,7 +8,7 @@ export default function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path ="/" render={() => <Login />}/>
-        <Route exact path ="/cadastro" render={() => <></>}/>
+        <Route exact path ="/cadastro" render={() => <Cadastro />}/>
         <Route exact path ="/habitos" render={() => <></>}/>
         <Route exact path ="/hoje" render={() => <></>}/>
         <Route exact path ="/historico" render={() => <></>}/>
---
+++ src/components/Cadastro.js
@@ -0,0 +1,68 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import logo from '../assets/logo.png';
+
+export default function Cadastro() {
+  return (
+    <>
+      <Wrapper>
+        <img src={logo} alt=""/>
+        <input placeholder="email" />
+        <input placeholder="senha" />
+        <input placeholder="nome" />
+        <input placeholder="foto" />
+        <button>Cadastrar</button>
+        <Link to="/">Já tem uma conta? Faça login!</Link>
+      </Wrapper>
+    </>
+  )
+}
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 68px 36px 0 36px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 30px;
+  gap: 6px;
+
+  img {
+    margin: 0 0 34px 0;
+  }
+
+  input {
+    font-family: "Lexend Deca", sans-serif;
+    width: 100%;
+    height: 45px;
+    border: 1px solid #D5D5D5;
+    border-radius: 5px;
+    outline: none;
+    font-size: 20px;
+    padding: 0 10px 0 10px;
+
+  }
+
+  input::placeholder {
+
+    color: #DBDBDB;
+  }
+
+  button {
+    background: #52B6FF;
+    width: 100%;
+    height: 45px;
+    border: none;
+    border-radius: 5px;
+    color: #FFFFFF;
+    font-size: 21px;
+  }
+  
+  a {
+    font-size: 14px;
+    color: #52B6FF;
+    margin-top: 19px;
+    word-wrap: break-word;
+  }
+`;
