f108d8693929a970f3a2e1a1e225e47c5284a45f
leandrodcs <eng.leandrodcs@gmail.com>
2021-09-08 15:20:34 -0300

    Login page layout added

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import '../reset.css';
+import Login from './Login';
 
 export default function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path ="/" render={() => <Login />}/>
+        <Route exact path ="/cadastro" render={() => <></>}/>
+        <Route exact path ="/habitos" render={() => <></>}/>
+        <Route exact path ="/hoje" render={() => <></>}/>
+        <Route exact path ="/historico" render={() => <></>}/>
+      </Switch>
+    </BrowserRouter>
   )
 }
